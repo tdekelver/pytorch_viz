f63a029d44fd2c8d6147b7760f2b68c5c6d4ea1e
Thomas Dekelver <thomas.dekelver@businessdecision.be>
2018-12-24 11:52:38 +0100

    Adding adaptation of vizualisation library for pytorch models in image recognition, adaptations to make work on ResNet in progress

---
+++ viz_pytorch/src/generate_class_specific_samples.py
@@ -0,0 +1,78 @@
+"""
+Created on Thu Oct 26 14:19:44 2017
+
+Original Author:
+Utku Ozbulak - github.com/utkuozbulak
+
+Changes made by:
+Thomas Dekelver - git.bdbelux.be/tdekelver
+"""
+import os
+import cv2
+import numpy as np
+
+from torch.optim import SGD
+from torchvision import models
+
+from misc_functions import preprocess_image, recreate_image, save_image
+
+
+class ClassSpecificImageGeneration():
+    """
+        Produces an image that maximizes a certain class with gradient ascent
+    """
+    def __init__(self, model, target_class,mean,std,lr = 6,min_loss = -1000,name = 'generated'):
+        self.mean = mean # mean of channels (in fastai: data.norm.keywords['mean'])
+        self.std = std # std of channels (in fastai: data.norm.keywords['std'])
+        self.lr = lr # initial learning rate to use (default 6)
+        self.min_loss = min_loss # min loss to obtain before stopping convergence (default -1000)
+        self.name = name # name of images to save
+        self.model = model
+        self.model.eval()
+        self.target_class = target_class
+        # Generate a random image
+        self.created_image = np.uint8(np.random.uniform(0, 255, (224, 224, 3)))
+        # Create the folder to export images if not exists
+        if not os.path.exists('../generated'):
+            os.makedirs('../generated')
+
+    def generate(self):
+        initial_learning_rate = self.lr
+        loss = 10
+        i = 0
+        while loss >= self.min_loss:
+            # Process image and return variable
+            self.processed_image = preprocess_image(self.created_image, False)
+            # Define optimizer for the image
+            optimizer = SGD([self.processed_image], lr=initial_learning_rate)
+            # Forward
+            output = self.model(self.processed_image)
+            # Target specific class
+            class_loss = -output[0, self.target_class]
+            if loss > class_loss.data.numpy():
+                loss = class_loss.data.numpy()
+            if i % 25 == 0:
+                print('Iteration:', str(i), 'Loss', "{0:.2f}".format(class_loss.data.numpy()))
+            # Zero grads
+            self.model.zero_grad()
+            # Backward
+            class_loss.backward()
+            # Update image
+            optimizer.step()
+            # Recreate image
+            self.created_image = recreate_image(self.processed_image)
+            if i % 25 == 0:
+                # Save image
+                im_path = '../generated/'+ str(self.name)+'_iteration_'+str(i)+'.jpg'
+                save_image(self.created_image, im_path)
+            i += 1
+        im_path = '../generated/'+ str(self.name)+'_iteration_final.jpg'
+        save_image(self.created_image,im_path)
+        return self.processed_image
+
+
+if __name__ == '__main__':
+    target_class = 130  # Flamingo
+    pretrained_model = models.resnet18(pretrained=True)
+    csig = ClassSpecificImageGeneration(pretrained_model, target_class)
+    csig.generate()
